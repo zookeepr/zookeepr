3ec5be8229babae37881b91772ec91b45bc0af54
Jamie Wilkinson <jaq@spacepants.org>
2006-04-25 12:44:03 +1000

    add people controller CRUD

--- zookeepr/controllers/people.py
+++ zookeepr/controllers/people.py
@@ -2,4 +2,16 @@ from zookeepr.lib.base import *
 
 class PeopleController(BaseController):
     def index(self):
+        # so the default actoin for a person is to view your own person, right?
         pass
+
+    def view(self, id):
+        m.write("you're viewing person %s" % id)
+
+    def edit(self, id):
+        m.write("you're editing person %s" % id)
+
+    def delete(self, id):
+        m.write("you're deleting person %s" % id)
+
+    def 
