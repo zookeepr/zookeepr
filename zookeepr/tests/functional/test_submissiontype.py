ed9384bca8bbaf942aa1de1079554a0f32e76b89
Jamie Wilkinson <jaq@spacepants.org>
2006-04-26 15:27:00 +1000

    add full test for testing the 'new submiossion' proecess

--- zookeepr/tests/functional/test_submissiontype.py
+++ zookeepr/tests/functional/test_submissiontype.py
@@ -11,11 +11,18 @@ class TestSubmissiontypeController(TestController):
 
         res.mustcontain('Name:')
 
-        res = self.app.get(url_for(controller='submissiontype', action='new'), params=dict(name='Paper'))
+        res = self.app.get(url_for(controller='submissiontype', action='new'), params=dict(name='Asterisk Talk'))
 
         # follow redirect
         res = res.follow()
 
-        res.mustcontain('view subtype Paper')
+        # viewing a subtype
+        res.mustcontain('view subtype')
 
-        
+        # check that it's in the database!
+        subs = SubmissionType.select_by(name='Asterisk Talk')
+        assert len(subs) == 1
+        sub = subs[0]
+
+        # check that we're viewing the correct id!
+        res.mustcontain('view subtype %d' % sub.id)
