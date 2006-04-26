650dcad6fa29efb94be1b864d63df127eb1499e9
Jamie Wilkinson <jaq@spacepants.org>
2006-04-26 13:05:57 +1000

    start adding submissiontype controller and tests

--- zookeepr/tests/functional/test_submissiontype.py
+++ zookeepr/tests/functional/test_submissiontype.py
@@ -4,3 +4,11 @@ class TestSubmissiontypeController(TestController):
     def test_index(self):
         response = self.app.get(url_for(controller='submissiontype'))
         # Test response...
+
+    def test_new(self):
+        res = self.app.get(url_for(controller='submissiontype'))
+        f = res.form[0]
+
+        f['name'] = 'Paper'
+
+        res = f.submit()
