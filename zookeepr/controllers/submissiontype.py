c14f5ef46c41841dcadd5a73f43b47bf92fd248c
Jamie Wilkinson <jaq@spacepants.org>
2006-04-27 18:21:50 +1000

    add lots of comments and update TODO

--- zookeepr/controllers/submissiontype.py
+++ zookeepr/controllers/submissiontype.py
@@ -3,29 +3,34 @@ from zookeepr.lib.base import *
 class SubmissiontypeController(BaseController):
     
     def index(self):
-        # index action lists
+        """Show a list of all submission types currently in the system."""
         # GET -> return list of subtypes
         # POST -> NOOP, do GET
 
-        # get submission types and assign to thingy
+        # get submission types and assign to the magical template global
         c.submissiontypes = model.SubmissionType.select()
-        
         m.subexec('submissiontype/list.myt')
 
     def view(self, id):
+        """View a specific submission type."""
         # GET -> return subtype
         # POST -> NOOP, do GET
 
         # assign to the template global
         c.submissiontype = model.SubmissionType.get(id)
-
-        # call the template
         m.subexec('submissiontype/view.myt')
 
     def edit(self, id):
-        # GET -> return 'edit' form
-        # POST -> update entry with form results
+        """Allow editing of a specific submission type.
+
+        GET requests return an 'edit' form, prefilled with the current
+        data.
+
+        POST requests update the SubmissionType with the data posted.
+        """
+        # get us this SubType
         st = model.SubmissionType.get(id)
+        # initialise variables
         defaults, errors = {}, {}
         
         # FIXME: gotta be a better way to seed the form
@@ -44,21 +49,34 @@ class SubmissiontypeController(BaseController):
         m.subexec('submissiontype/edit.myt', defaults=defaults, errors=errors)
 
     def delete(self, id):
-        # GET -> return 'delete' formm
-        # POST -> act on results of delete form
+        """Delete a submission type.
+
+        GET will return a form asking for approval.
+
+        POST requests with a key 'delete' set to 'ok' will delete the item.
+
+        Invalid ids will return a 404 not found.
+        """
         errors, defaults = {}, m.request_args
+        
         #h.log(defaults)
         if defaults:
             #h.log(defaults)
             st = model.SubmissionType.get(id)
+            h.log(st)
             st.delete()
             st.commit()
             return h.redirect_to(action='index', id=None)
         m.subexec('submissiontype/delete.myt', defaults=defaults, errors=errors)
 
     def new(self):
-        # GET -> return 'new' form
-        # POST -> create new with results of form
+        """Create a new submission type.
+
+        GET requests will return a blank form for submitting all attributes.
+
+        POST requests will create the submission type, and return a redirect
+        to view the new SubType.
+        """
         errors, defaults = {}, m.request_args
         if defaults:
             # create, etc
