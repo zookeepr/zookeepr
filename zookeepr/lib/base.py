c774ea8c0981a8a06f2c5bccd93b222df2bbcc0d
Jamie Wilkinson <jaq@spacepants.org>
2006-04-30 16:33:33 +1000

    add FormEncode to the requirements and use it to decode request args automagically

--- zookeepr/lib/base.py
+++ zookeepr/lib/base.py
@@ -1,7 +1,20 @@
 from pylons import Controller, m, h, c, g, session, request, params
+from formencode.variabledecode import variable_decode
 import zookeepr.models as model
+#import sqlalchemy
 
 class BaseController(Controller):
     def __call__(self, action, **params):
         # Insert any code to be run per request here
+
+        # Connect the ORM to the database
+        #sqlalchemy.global_connect(g.pylons_config.app_conf['dburi'])
+        # clear the objectstore session
+        #sqlalchemy.objectstore.clear()
+
+        # Use FormEncode to decode the request args automagically
+        if m.request_args:
+            m.request_args = variable_decode(m.request_args)
+
+        # Let the base class defer to controller actions
         Controller.__call__(self, action, **params)
