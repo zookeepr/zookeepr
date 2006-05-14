801d439b8962edf802a44a20e342c07b33a0aa25
Jamie Wilkinson <jaq@spacepants.org>
2006-05-14 23:00:35 +1000

    update the person schema validator

--- zookeepr/models/forms.py
+++ zookeepr/models/forms.py
@@ -4,14 +4,19 @@ from formencode.validators import *
 from contentstor import FormSchema
 
 class PersonSchema(FormSchema):
-    handle = String(not_empty=True)
+    handle = PlainText(not_empty=True)
     email_address = String()
     password = String()
-
+    password_confirm = String()
+    
     firstname = String()
     lastname = String()
     phone = String()
     fax = String()
+
+    chained_validators = [
+        FieldsMatch('password', 'password_confirm')
+        ]
 
 class SubmissionTypeSchema(FormSchema):
     name = String(maxlength=40)
