184aaf0cb61adb181a2542ba5cfb9e1871a3c80d
Jamie Wilkinson <jaq@spacepants.org>
2006-05-15 00:33:36 +1000

    move the crappy db setup back into app_globals

--- zookeepr/websetup.py
+++ zookeepr/websetup.py
@@ -1,24 +1,12 @@
 import os
 
 from paste.deploy import appconfig
-import sqlalchemy
 
 from lib.app_globals import Globals
 
 def setup_config(command, filename, section, vars):
     """Place any commands to set up zookeepr here.
     """
-
-    # FIXME: this method for creating the tables if the databsae was just created is not very
-    # robust; currently it's trapping an exception created by pysqlite2
-    try:
-        model.person.create()
-        model.submission_type.create()
-        model.submission.create()
-    except sqlalchemy.SQLError, e:
-        # we only want to pass on operational errors
-        if e.args[0].find('table person already exists') == -1:
-            raise e
 
     # The connection to the AuthKit store is made in ``app_globals.py`` but
     # that doesn't mean the necessary tables, users, roles, etc have been
