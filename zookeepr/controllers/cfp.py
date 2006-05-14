75b3dab7999845b184d94aa3fabb2ec884f5a559
Jamie Wilkinson <jaq@spacepants.org>
2006-05-14 21:48:05 +1000

    clear the objectstore at the start of a submission

--- zookeepr/controllers/cfp.py
+++ zookeepr/controllers/cfp.py
@@ -4,6 +4,9 @@ class CfpController(BaseController):
     """Controller for submitting something to the conference"""
     def index(self):
         """Create a new submission"""
+
+        objectstore.clear()
+        
         new_person = model.Person()
         new_submission = model.Submission()
 
@@ -12,11 +15,16 @@ class CfpController(BaseController):
             new_submission.update(**m.request_args['submission'])
             new_submission.person = new_person
 
+            print new_person.handle
+
             if new_person.validate() and new_submission.validate():
                 # save to database
                 objectstore.flush()
                 return h.redirect_to(controller='person', action='view', id=new_person.handle)
+            else:
+                objectstore.clear()
 
+        # set up for the cfp form
         c.submissiontypes = model.SubmissionType.select()
         c.person = new_person
         setattr(c, 'submission', new_submission)
