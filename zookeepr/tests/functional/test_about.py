cb538d969dc3111939d956294a7827e44f14e123
Ben Leslie <benno@benno.id.au>
2006-06-01 17:35:23 +1000

    TestContoller now called ControllerTest.

--- zookeepr/tests/functional/test_about.py
+++ zookeepr/tests/functional/test_about.py
@@ -1,6 +1,6 @@
 from zookeepr.tests.functional import *
 
-class TestAboutController(TestController):
+class TestAboutController(ControllerTest):
     def test_index(self):
         response = self.app.get(url_for(controller='/about'))
         # Test response...
