ed1a068e3660828f40c8f0705ff7f1f535299e24
Jamie Wilkinson <jaq@spacepants.org>
2006-06-13 21:09:03 +1000

    make one of them generic controllers with a form validator, and it'll blow up becuase there's no model yet

--- zookeepr/controllers/cfp.py
+++ zookeepr/controllers/cfp.py
@@ -1,5 +1,15 @@
+from formencode import validators, compound
+from formencode.schema import Schema
+
 from zookeepr.lib.base import *
 
-class CfpController(BaseController):
-    def index(self):
-        m.subexec("cfp/new.myt")
+class CFPValidator(Schema):
+    pass
+
+class CfpController(BaseController, View, Modify):
+    validator = {
+        'new': CFPValidator,
+        }
+
+    model = model.CFP
+    individual = 'cfp'
