193af7fe0e328f37afa4169c4f65d87a2fc9168b
Jamie Wilkinson <jaq@spacepants.org>
2006-06-13 20:34:38 +1000

    just let subexec decide whether we 404 or not

--- zookeepr/controllers/about.py
+++ zookeepr/controllers/about.py
@@ -1,10 +1,11 @@
 from zookeepr.lib.base import *
 
 class AboutController(BaseController):
-    templates = ["index", "cfp", "contact", "sponsors"]
+    #templates = ["index", "cfp", "contact", "sponsors"]
 
     def view(self, id):
-        if id in self.templates:
-            m.subexec("about/%s.myt" % id)
-        else:
-            raise AttributeError, id
+        #if id in self.templates:
+        #    m.subexec("about/%s.myt" % id)
+        #else:
+        #    m.abort(404, "Object not found")
+        m.subexec("about/%s.myt" % id)
