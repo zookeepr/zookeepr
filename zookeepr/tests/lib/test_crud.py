a9259eb8ccdbc31ec11136f2ad5851e45911cedc
Jamie Wilkinson <jaq@spacepants.org>
2006-06-17 18:31:50 +1000

    the tests for lib/crud don't work so well, comment them out

--- zookeepr/tests/lib/test_crud.py
+++ zookeepr/tests/lib/test_crud.py
@@ -1,27 +1,12 @@
 from unittest import TestCase
 
-from zookeepr.lib.crud import CRUDBase, Create
+from zookeepr.lib.crud import Create
 
-class TestCRUD(TestCase):
-    def test_redirect_to(self):
+# class TestCRUD(TestCase):
+#     def test_create(self):
 
-        target = dict(controller='pants', action='index')
-
-        class A(CRUDBase):
-            pass
-        
-        class B(CRUDBase):
-            redirect_map = dict(new=target)
-
-        default_redirect = dict(controller='a', action='view', id=1)
-        a = A()
-        result = a.redirect_to('new', default_redirect)
-
-        print "a", result
-        
-        c = C()
-        result = c.redirect_to('new', default_redirect)
-        print "c", result
-
-    def test_create(self):
-        pass
+#         class 
+#         class A(Create):
+#             model_name = 'A'
+#         c = Create
+#         pass
