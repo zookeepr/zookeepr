89d4e69894ef9dcc740c71645526fac59ede9068
Jamie Wilkinson <jaq@spacepants.org>
2006-06-28 07:50:12 +1000

    don't worry if it's not scriptable

--- zookeepr/lib/validators.py
+++ zookeepr/lib/validators.py
@@ -8,10 +8,6 @@ class BaseSchema(Schema):
     def validate(self, input):
         try:
             result = self.to_python(input)
-            # to_python returns None if input is empty, but we want
-            # a subscriptable object
-            if result is None:
-                result = {}
             return result, {}
         except Invalid, e:
             return {}, e.unpack_errors()
