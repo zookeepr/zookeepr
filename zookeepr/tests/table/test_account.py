4b2607247f95d3e494aafbf3054fd136a4884372
Jamie Wilkinson <jaq@spacepants.org>
2006-07-18 12:46:36 +1000

    fix typos

--- zookeepr/tests/table/test_account.py
+++ zookeepr/tests/table/test_account.py
@@ -8,7 +8,7 @@ class TestAccount(TableTest):
     accounts are correctly activated before use.
 
     Personal details associated with this account (i.e. handle, full name,
-    address, phone numner, etc) are kept in the ``person`` table, which is
+    address, phone number, etc) are kept in the ``person`` table, which is
     linked here via a foreign key.
 
     Users log in with only their ``email_address`` and a ``password``, which
@@ -18,7 +18,7 @@ class TestAccount(TableTest):
     samples = [dict(email_address='testguy@example.org',
                     password_hash='test',
                     activated=False),
-               dict(email_address='testgirl@examplr.com',
+               dict(email_address='testgirl@example.com',
                     password_hash='p4ssw0rd',
                     activated=True),
                ]
