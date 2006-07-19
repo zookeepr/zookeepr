325d521abd24a5a374bc0bf25c38d01dafd1c201
Jamie Wilkinson <jaq@spacepants.org>
2006-07-19 21:29:37 +1000

    uncomment and refactor cfp test

--- zookeepr/tests/functional/test_cfp.py
+++ zookeepr/tests/functional/test_cfp.py
@@ -3,53 +3,71 @@ import md5
 from zookeepr.tests.functional import *
 
 class TestCFP(ControllerTest):
-#     model = model.CFP
-#     name = 'cfp'
-#     url = '/cfp'
-#     samples = [dict(email_address='testguy@example.org',
-#                     password='password',
-#                     password_confirm='password',
-#                     title='title yo',
-#                     abstract='abstract yo',
-#                     type=1,
-#                     experience="some",
-#                     url='http://example.org',
-#                     attachment='foo',
-#                     assistance=True,
-#                     ),
-#                dict(email_address='testgirl@example.org',
-#                     password='snuh',
-#                     password_confirm='snuh',
-#                     title='some title',
-#                     abstract='some abstract',
-#                     type=2,
-#                     experience="none",
-#                     url='http://example.com',
-#                     attachment='bar',
-#                     assistance=False,
-#                     ),
-#                ]
-#     no_test = ['password_confirm']
-#     mangles = dict(password = lambda p: md5.new(p).hexdigest(),
-#                    attachment = lambda a: buffer(a),
-#                    )
+    def test_create(self):
+        url = url_for(controller='cfp', action='new')
 
-#     def setUp(self):
-#         ControllerTest.setUp(self)
-#         st1 = model.SubmissionType('Paper')
-#         st2 = model.SubmissionType('Scissors')
-#         self.session.save(st1)
-#         self.session.save(st2)
-#         self.session.flush()
-#         self.stid = (st1.id, st2.id)
+        response = self.app.get(url)
+        form = response.form
 
-#     def tearDown(self):
-#         st1 = self.session.get(model.SubmissionType, self.stid[0])
-#         st2 = self.session.get(model.SubmissionType, self.stid[1])
-#         self.session.delete(st1)
-#         self.session.delete(st2)
-#         self.session.flush()
-#         ControllerTest.tearDown(self)
+        print form.text
+        print form.fields
+
+        reg_data = {'email_address': 'testguy@example.org',
+                    'password': 'password',
+                    'password_confirm': 'password',
+                   }
+        sub_data = {'title': 'title',
+                    'abstract': 'abstract',
+                    'type': 1,
+                    'experience': 'some',
+                    'url': 'http://example.org',
+                    'attachment': 'foo',
+                    'assistance': True,
+                    }
+        for k in reg_data.keys():
+            form['registration.' + k] = reg_data[k]
+        for k in sub_data.keys():
+            form['submission.' + k] = sub_data[k]
+
+        form.submit()
+
+        regs = self.session.query(Registration).select()
+        self.assertEqual(1, len(regs))
+
+        for key in reg_data.keys():
+            self.check_attribute(regs[0], key, reg_data[key])
+
+        subs = self.session.query(Submission).select()
+        self.assertEqual(1, len(subs))
+
+        for key in sub_data.keys():
+            self.check_attribute(subs[0], key, sub_data[key])
+
+        self.session.delete(regs[0])
+        self.session.delete(subs[0])
+        self.session.flush()
+
+    no_test = ['password_confirm']
+    mangles = dict(password = lambda p: md5.new(p).hexdigest(),
+                   attachment = lambda a: buffer(a),
+                   )
+
+    def setUp(self):
+        ControllerTest.setUp(self)
+        st1 = model.SubmissionType('Paper')
+        st2 = model.SubmissionType('Scissors')
+        self.session.save(st1)
+        self.session.save(st2)
+        self.session.flush()
+        self.stid = (st1.id, st2.id)
+
+    def tearDown(self):
+        st1 = self.session.get(model.SubmissionType, self.stid[0])
+        st2 = self.session.get(model.SubmissionType, self.stid[1])
+        self.session.delete(st1)
+        self.session.delete(st2)
+        self.session.flush()
+        ControllerTest.tearDown(self)
                            
         
     def test_cfp_registration(self):
