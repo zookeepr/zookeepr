161e8a63c1b9d4b15a9fcccfdace32bb3d5e2ea2
Jamie Wilkinson <jaq@spacepants.org>
2006-07-19 16:31:06 +1000

    cleanup

--- zookeepr/tests/functional/test_home.py
+++ zookeepr/tests/functional/test_home.py
@@ -2,6 +2,4 @@ from zookeepr.tests.functional import *
 
 class TestHomeController(ControllerTest):
     def test_index(self):
-        """Test FIXME: does nothing"""
-        response = self.app.get(url_for(controller='/home'))
-        # Test response...
+        response = self.app.get(url_for(controller='home'))
