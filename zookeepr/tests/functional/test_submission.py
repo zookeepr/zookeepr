5077eddbc2bc08b19c4d940d0644b0fe63d8d227
Jamie Wilkinson <jaq@spacepants.org>
2006-07-19 21:16:26 +1000

    uncomment submission test and controller

--- zookeepr/tests/functional/test_submission.py
+++ zookeepr/tests/functional/test_submission.py
@@ -1,15 +1,15 @@
 from zookeepr.tests.functional import *
 
-# class TestSubmission(ControllerTest):
-#     model = model.Submission
-#     name = 'submission'
-#     url = '/submission'
-#     samples = [dict(title='test',
-#                     abstract='abstract 1',
-#                     experience='experience 1',
-#                     ),
-#                dict(title='not a test',
-#                     abstract='abstract 2',
-#                     experience='experience 2',
-#                     ),
-#                ]
+class TestSubmission(ControllerTest):
+    model = model.Submission
+    name = 'submission'
+    url = '/submission'
+    samples = [dict(title='test',
+                    abstract='abstract 1',
+                    experience='experience 1',
+                    ),
+               dict(title='not a test',
+                    abstract='abstract 2',
+                    experience='experience 2',
+                    ),
+               ]
