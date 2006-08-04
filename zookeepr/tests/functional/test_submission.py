594b47ab44d75edb584c0147428952bc5de990df
Jamie Wilkinson <jaq@spacepants.org>
2006-08-04 17:58:01 +1000

    update submission controller test to create submission types during setup

--- zookeepr/tests/functional/test_submission.py
+++ zookeepr/tests/functional/test_submission.py
@@ -7,9 +7,29 @@ class TestSubmission(ControllerTest):
     samples = [dict(title='test',
                     abstract='abstract 1',
                     experience='experience 1',
+                    url='',
+                    submission_type_id=1,
                     ),
                dict(title='not a test',
                     abstract='abstract 2',
                     experience='experience 2',
+                    submission_type_id=1,
+                    url='',
                     ),
                ]
+
+    def setUp(self):
+        ControllerTest.setUp(self)
+        model.submission_type.insert().execute(
+            dict(id=1, name='Paper'),
+            )
+        model.submission_type.insert().execute(
+            dict(id=2, name='Presentation'),
+            )
+        model.submission_type.insert().execute(
+            dict(id=3, name='Miniconf'),
+            )
+
+    def tearDown(self):
+        model.submission_type.delete().execute()
+        ControllerTest.tearDown(self)
