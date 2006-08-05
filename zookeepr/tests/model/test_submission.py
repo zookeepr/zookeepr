f40d62afb007884b44bf5fc1dcf48f00c88a8071
Jamie Wilkinson <jaq@spacepants.org>
2006-08-05 13:35:14 +1000

    changed the interface to submission data model

--- zookeepr/tests/model/test_submission.py
+++ zookeepr/tests/model/test_submission.py
@@ -26,7 +26,8 @@ class TestSubmission(ModelTest):
         session.save(v)
         session.flush()
 
-        s = model.Submission('Venal Versimilitude: Vast vocation or violition of volition?',
+        s = model.Submission(1,
+                             'Venal Versimilitude: Vast vocation or violition of volition?',
                              st.id, #FIXME: this exposes knowledge of the table structure to the data model
                              'This visage, no mere veneer of vanity, is it vestige of the vox populi, now vacant, vanished, as the once vital voice of the verisimilitude now venerates what they once vilified. However, this valorous visitation of a by-gone vexation, stands vivified, and has vowed to vanquish these venal and virulent vermin vanguarding vice and vouchsafing the violently vicious and voracious violation of volition. The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day vindicate the vigilant and the virtuous. Verily, this vichyssoise of verbiage veers most verbose vis-a-vis an introduction, and so it is my very good honor to meet you and you may call me V.',
                              'Vaudeville',
