38e592af6d0bac4a83ae6d5a4c2e43abeb5c6835
Jamie Wilkinson <jaq@spacepants.org>
2006-08-05 18:04:42 +1000

    switch submisison controller to securecontroller

--- zookeepr/controllers/submission.py
+++ zookeepr/controllers/submission.py
@@ -1,6 +1,7 @@
 from formencode import validators, compound, schema, variabledecode
 
-from zookeepr.lib.base import BaseController, c
+from zookeepr.lib.auth import SecureController
+from zookeepr.lib.base import c
 from zookeepr.lib.crud import Modify, View
 from zookeepr.lib.validators import BaseSchema
 from zookeepr.models import Submission, SubmissionType
@@ -20,7 +21,7 @@ class EditSubmissionSchema(BaseSchema):
     submission = SubmissionSchema()
     pre_validators = [variabledecode.NestedVariables]
 
-class SubmissionController(BaseController, View, Modify):
+class SubmissionController(SecureController, View, Modify):
     validators = {"new" : NewSubmissionSchema(),
                   "edit" : EditSubmissionSchema()}
 
@@ -28,6 +29,6 @@ class SubmissionController(BaseController, View, Modify):
     individual = 'submission'
 
     def __before__(self, **kwargs):
-        BaseController.__before__(self, **kwargs)
+        SecureController.__before__(self, **kwargs)
         
         c.submission_types = self.objectstore.query(SubmissionType).select()
