5e76782865b5d8b2c988e2665847515ed1790b1d
Jamie Wilkinson <jaq@spacepants.org>
2006-08-06 14:42:48 +1000

    we don't use roles yet, so comment out the test

--- zookeepr/tests/model/core/tables/test_role.py
+++ zookeepr/tests/model/core/tables/test_role.py
@@ -1,13 +1,13 @@
-from zookeepr.tests.model.table import *
-
-class TestRole(TableTest):
-    """Test the ``role`` table.
-
-    This table describes roles to use with an ACL system.
-    """
-    table = 'core.tables.role'
-    samples = [dict(name='test'),
-               dict(name='test1'),
-               ]
-    not_nullables = ['name']
-    uniques = ['name']
+#from zookeepr.tests.model.table import *
+#
+#class TestRole(TableTest):
+#    """Test the ``role`` table.
+#
+#    This table describes roles to use with an ACL system.
+#    """
+#    table = 'core.tables.role'
+#    samples = [dict(name='test'),
+#               dict(name='test1'),
+#               ]
+#    not_nullables = ['name']
+#    uniques = ['name']
