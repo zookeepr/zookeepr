b58faac622cb9e47e473b4ca8ffe5a502728083c
Jamie Wilkinson <jaq@spacepants.org>
2006-08-08 17:25:33 +1000

    reformat person mapper

--- zookeepr/model/core/mapper.py
+++ zookeepr/model/core/mapper.py
@@ -5,5 +5,7 @@ from zookeepr.model.core.domain import Person
 
 # Map the Person object onto both the account and person tables
 mapper(Person, join(account, person),
-    properties = {'account_id': [account.c.id, person.c.account_id]}
+    properties = {
+        'account_id': [account.c.id, person.c.account_id],
+    }
     )
