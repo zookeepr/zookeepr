ef9669e3ae3fe2014fec62a3130e2df0a00fb6b0
Jamie Wilkinson <jaq@spacepants.org>
2006-08-10 15:34:25 +1000

    add failing test for roles

--- zookeepr/tests/model/core/domain/test_role.py
+++ zookeepr/tests/model/core/domain/test_role.py
@@ -1,41 +1,46 @@
-#from zookeepr.tests.model import *
-#
-#class TestRoleModel(ModelTest):
-#     model = 'Role'
-#     samples = [dict(name='site admin'),
-#                dict(name='speaker'),
-#                ]
-#     not_nullables = ['name']
-#     uniques = ['name']
-#
-#     def test_map_person(self):
-#         """Test mapping persons to roles"""
-#         session = create_session()
-#
-#         p = model.core.Person(handle='testguy',
-#                          email_address='testguy@example.org')
-#         r = model.Role('admin')
-#         
-#         session.save(p)
-#         session.save(r)
-#         
-#         p.roles.append(r)
-#
-#         session.flush()
-#         
-#         rid = r.id
-#         pid = p.id
-#         
-#         session.clear()
-#         
-#         p = session.get(model.core.Person, pid)
-#         self.assertEqual(['admin'], [r.name for r in p.roles])
-#         
-#         # clean up
-#         session.delete(p)
-#         session.delete(session.get(model.Role, rid))
-#         session.flush()
-#         
-#         # check
-#         self.assertEqual(0, len(session.query(model.Role).select()))
-#         self.check_empty_session()
+from zookeepr.model import Person, Role
+from zookeepr.tests.model import *
+
+class TestRoleModel(ModelTest):
+     model = 'Role'
+     samples = [dict(name='site admin'),
+                dict(name='speaker'),
+                ]
+     not_nullables = ['name']
+     uniques = ['name']
+
+     def test_map_person(self):
+         """Test mapping persons to roles"""
+         session = create_session()
+
+         p = Person(handle='testguy',
+                    email_address='testguy@example.org')
+         r = Role('admin')
+
+         p.save()
+         r.save()
+         p.flush()
+         r.flush()
+         
+         p.roles.append(r)
+
+         p.save()
+         p.flush()
+         
+         rid = r.id
+         pid = p.id
+         
+         objectstore.clear()
+         
+         p = Person.get(pid)
+         self.assertEqual(['admin'], [r.name for r in p.roles])
+         
+         # clean up
+         p.delete()
+         p.flush()
+         Role.get(rid).delete()
+         Role.get(rid).flush()
+         
+         # check
+         self.assertEqual(0, len(Role.select()))
+         self.check_empty_session()
