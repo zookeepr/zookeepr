6a32493fe5278e99289384ffd9d1057d650900d4
Jamie Wilkinson <jaq@spacepants.org>
2006-08-10 17:04:07 +1000

    test that the roles attributes on person behaves correctly

--- zookeepr/tests/model/core/domain/test_role.py
+++ zookeepr/tests/model/core/domain/test_role.py
@@ -63,3 +63,61 @@ class TestRoleModel(ModelTest):
          # check
          self.assertEqual(0, len(Role.select()))
          self.check_empty_session()
+
+     def test_many_roles(self):
+         p1 = Person(email_address='one@example.org', password='1')
+         p1.save()
+         p2 = Person(email_address='two@example.org', password='2')
+         p2.save()
+         p3 = Person(email_address='three@example.org', password='3')
+         p3.save()
+         p4 = Person(email_address='four@example.org', password='4')
+         p4.save()
+
+         r1 = Role('single')
+         r1.save()
+         r1.flush()
+         r2 = Role('double')
+         r2.save()
+         r2.flush()
+
+         p1.roles.append(r1)
+         p2.roles.append(r2)
+         p4.roles.append(r2)
+
+         objectstore.flush()
+         p = (p1.id, p2.id, p3.id, p4.id)
+         r = (r1.id, r2.id)
+
+         objectstore.clear()
+
+         p1 = Person.get(p[0])
+         p2 = Person.get(p[1])
+         p3 = Person.get(p[2])
+         p4 = Person.get(p[3])
+         r1 = Role.get(r[0])
+         r2 = Role.get(r[1])
+
+         # test
+         self.failUnless(r1 in p1.roles)
+         self.failUnless(r2 in p2.roles)
+         self.failUnless(r2 in p4.roles)
+         self.assertEqual([], p3.roles)
+
+         self.failIf(r1 in p2.roles)
+         self.failIf(r1 in p3.roles)
+         self.failIf(r1 in p4.roles)
+
+         self.failIf(r2 in p1.roles)
+         self.failIf(r2 in p3.roles)
+
+         # clean up
+         p1.delete()
+         p2.delete()
+         p3.delete()
+         p4.delete()
+
+         r1.delete()
+         r2.delete()
+
+         objectstore.flush()
