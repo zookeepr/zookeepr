7d689a9418a06bd3c7b8ce62812379d886f07f8e
Jamie Wilkinson <jaq@spacepants.org>
2006-08-11 23:16:40 +1000

    uncomment and make test pass

--- zookeepr/tests/functional/test_home.py
+++ zookeepr/tests/functional/test_home.py
@@ -5,31 +5,37 @@ class TestHomeController(ControllerTest):
     def test_index(self):
         response = self.app.get(url_for(controller='home'))
 
-#     def test_index_logged_in(self):
-#         p = Person(email_address='testguy@testguy.org',
-#                    password='test',
-#                    firstname='Testguy')
-#         p.save()
-#         print p
-#         #s = Submission(title='foo')
-#         # s.save()
-#         # p.submissions.append(s)
-#         objectstore.flush()
+    def test_index_logged_in(self):
+        p = Person(email_address='testguy@example.org',
+                   password='test',
+                   firstname='Testguy')
+        p.activated = True
+        p.save()
+        print p
+        s = Submission(title='foo')
+        s.save()
+        p.submissions.append(s)
+        p.flush()
+        s.flush()
 
-#         print p
+        print p
 
-# #         resp = self.app.get(url_for(controller='account',action='signin'))
-# #         f = resp.form
-# #         f['email_address'] = 'testguy@example.org'
-# #         f['password'] = 'test'
-# #         resp = f.submit()
-# #         self.failUnless('person_id' in resp.session)
-# #         resp = resp.follow()
-# #         self.assertEqual('/', resp.url)
-# #         resp.mustcontain("Welcome, <strong>Testguy</strong>!")
-# #         resp.mustcontain("foo")
+        resp = self.app.get(url_for(controller='account',action='signin'))
+        f = resp.form
+        f['email_address'] = 'testguy@example.org'
+        f['password'] = 'test'
+        resp = f.submit()
+        print resp
+        print resp.session
+        self.failUnless('person_id' in resp.session)
+        self.assertEqual(p.id,
+                         resp.session['person_id'])
+        resp = resp.follow()
+        print resp.request.url
+        self.assertEqual('/', resp.request.url)
+        resp.mustcontain("Welcome, <strong>Testguy</strong>!")
+        resp.mustcontain("foo")
         
-        
-#         p.delete()
-# #         s.delete()
-#         objectstore.flush()
+        p.delete()
+        s.delete()
+        objectstore.flush()
