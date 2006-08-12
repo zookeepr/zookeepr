28e5e549d2461431a4fb53b9991a38683dcb198b
Jamie Wilkinson <jaq@spacepants.org>
2006-08-12 17:19:32 +1000

    holy shit we weren't even using this crap

--- zookeepr/lib/base.py
+++ zookeepr/lib/base.py
@@ -28,20 +28,6 @@ class BaseController(WSGIController):
         # So we save it at initialisation and restore it each request
         default_metadata.context._engine = g.engine
 
-        # Convert the request_args into something sane. Basically what
-        # I am doing here is finding anything encoded as FieldStorage
-        # rather than a plain string and then encoding it as either a
-        # a string, or a file
-        request_args = dict(request.POST)
-        if request_args:
-            for key in request_args:
-                if isinstance(request_args[key], cgi.FieldStorage):
-                    if request_args[key].file and \
-                           type(request_args[key].file) == types.FileType:
-                        request_args[key] = request_args[key].value
-                    else:
-                        request_args[key] = request_args[key].value
-
         # I really don't know what I'm doing; trapped in a maze of twisty
         # little method-resolution-orders.
         # Apparently things that don't have __before__ will still try to
