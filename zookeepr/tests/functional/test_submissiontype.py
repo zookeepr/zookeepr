10bbb98397323ff2b93fb86e59a9aee7554b944a
Jamie Wilkinson <jaq@spacepants.org>
2006-08-13 10:04:41 +1000

    add failing test for submissiontype controller lockdown

--- zookeepr/tests/functional/test_submissiontype.py
+++ zookeepr/tests/functional/test_submissiontype.py
@@ -1,8 +1,26 @@
+from zookeepr.model import SubmissionType
 from zookeepr.tests.functional import *
 
 class TestSubmissionType(ControllerTest):
-    model = model.submission.domain.SubmissionType
+    model = SubmissionType
     name = 'submissiontype'
     url = '/submissiontype'
     samples = [dict(name='Paper'),
                dict(name='BOF')]
+
+    def test_submission_view_lockdown(self):
+        # we got one person
+        self.log_in()
+        # create a submission_type
+        st = SubmissionType('foo')
+        st.save()
+        st.flush()
+        # try to view the submission, we're not a site-admin
+        resp = self.app.get(url_for(controller='submissiontype',
+                                    action='view',
+                                    id=st.id),
+                            status=403)
+        # clean up
+        st.delete()
+        st.flush()
+        self.log_out()
