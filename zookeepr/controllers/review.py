486dad7c02055b41c6a59e0889c10691fabc368b
Jamie Wilkinson <jaq@spacepants.org>
2006-09-02 19:07:14 +1000

    turn on authorisation on the review controller

--- zookeepr/controllers/review.py
+++ zookeepr/controllers/review.py
@@ -1,7 +1,8 @@
-from zookeepr.lib.base import BaseController, render_response, model
+from zookeepr.lib.auth import AuthRole, SecureController
+from zookeepr.lib.base import render_response, model
 from zookeepr.lib.crud import List
 
-class ReviewController(BaseController, List):
+class ReviewController(SecureController, List):
     model = model.Review
     individual = 'review'
     permissions = {'index': [AuthRole('reviewer')],
