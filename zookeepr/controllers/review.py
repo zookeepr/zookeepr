e11012d98109acf56ff369cf63e8268065c43dc6
Jamie Wilkinson <jaq@spacepants.org>
2006-09-02 18:59:07 +1000

    only allow reviewers to look at the review index

--- zookeepr/controllers/review.py
+++ zookeepr/controllers/review.py
@@ -4,3 +4,5 @@ from zookeepr.lib.crud import List
 class ReviewController(BaseController, List):
     model = model.Review
     individual = 'review'
+    permissions = {'index': [AuthRole('reviewer')],
+                   }
