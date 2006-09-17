1740bb8ac8bf611198e7a62c40857257359afd9c
Jamie Wilkinson <jaq@spacepants.org>
2006-09-17 11:02:22 +1000

    stub tests for review features

---
+++ zookeepr/tests/functional/test_review.py
@@ -0,0 +1,29 @@
+from zookeepr.tests.functional import *
+
+class TestReviewBase(SignedInControllerTest):
+
+    def test_review_feedback(self):
+        """Test that one can put in optional feedback to the submitter from the review interface.
+        """
+        self.fail("untested")
+
+    def test_review_interface(self):
+        """Test that the interface shows two lists, one of unreviewed proposals, and one of reviewed proposals"""
+        self.fail("untested")
+
+    def test_review_interface_sorted(self):
+        """Test that the reviewed proposals are sorted by rank"""
+        self.fail("untested")
+
+    def test_reviews_isolated(self):
+        """Test that a reviewer can only see their own reviews"""
+        self.fail("untested")
+
+    def test_reviewer_name_hidden_from_submitter(self):
+        """Test taht a revier is anonymouse to submitters"""
+        self.fail("untested")
+
+    def test_reviewer_cant_review_own_proposal(self):
+        """Test that a reviewer can't review their own submissions."""
+        self.fail("untedted")
+
