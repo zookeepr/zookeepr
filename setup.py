c364aede640723ae54653f96fed1a1608c0f5f25
Jamie Wilkinson <jaq@spacepants.org>
2006-09-17 22:19:10 +1000

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zookeepr',
-    version="0.1.8",
+    version="0.1.8.1",
     #description="",
     #author="",
     #author_email="",
