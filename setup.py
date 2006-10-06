9605017d1094ad51b9194322739b15e931d74a0b
Jamie Wilkinson <jaq@spacepants.org>
2006-10-07 07:14:21 +1000

    next version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zookeepr',
-    version="0.1.12",
+    version="0.1.13",
     #description="",
     #author="",
     #author_email="",
