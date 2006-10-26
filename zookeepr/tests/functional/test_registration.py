1f8a875ea480d4fb1642057e53d1e685f89dcc7e
Jamie Wilkinson <jaq@spacepants.org>
2006-10-27 07:28:09 +1000

    call superclass tearDown, reindent

--- zookeepr/tests/functional/test_registration.py
+++ zookeepr/tests/functional/test_registration.py
@@ -4,47 +4,45 @@ class TestRegistrationController(ControllerTest):
     model = model.registration.Registration
     url = '/registration'
     param_name = 'registration'
-    samples = [dict(
-        registration=dict(address1='a1',
-            address2='a2',
-            city='city',
-            state='state1',
-            country='country1',
-            postcode='postcode1',
-            company='company1',
-            shelltext='shelltext1',
-            editortext='editortext1',
-            distrotext='distrotext1',
-            type='Professional',
-            discount_code='discount_code1',
-            teesize='M_M',
-            diet='diet1',
-            special='special1',
-            opendaydrag=1,
-            partner_email='partneremail1',
-            kids_0_3=1,
-            kids_4_6=1,
-            kids_7_9=1,
-            kids_10=1,
-            checkin=14,
-            checkout=20,
-            lasignup=True,
-            announcesignup=True,
-            delegatesignup=False,
-            editor='-',
-            distro='-',
-            shell='-',
-            accommodation='own',
-            ),
-            person=dict(
-
-            email_address='testguy@example.org',
-            password='test',
-            password_confirm='test',
-            handle='testguy',
-            fullname='testguy mctest',
-            )
-            )]
+    samples = [dict(registration=dict(address1='a1',
+                                      address2='a2',
+                                      city='city',
+                                      state='state1',
+                                      country='country1',
+                                      postcode='postcode1',
+                                      company='company1',
+                                      shelltext='shelltext1',
+                                      editortext='editortext1',
+                                      distrotext='distrotext1',
+                                      type='Professional',
+                                      discount_code='discount_code1',
+                                      teesize='M_M',
+                                      diet='diet1',
+                                      special='special1',
+                                      opendaydrag=1,
+                                      partner_email='partneremail1',
+                                      kids_0_3=1,
+                                      kids_4_6=1,
+                                      kids_7_9=1,
+                                      kids_10=1,
+                                      checkin=14,
+                                      checkout=20,
+                                      lasignup=True,
+                                      announcesignup=True,
+                                      delegatesignup=False,
+                                      editor='-',
+                                      distro='-',
+                                      shell='-',
+                                      accommodation='own',
+                                      ),
+                    person=dict(email_address='testguy@example.org',
+                                password='test',
+                                password_confirm='test',
+                                handle='testguy',
+                                fullname='testguy mctest',
+                                )
+                    )
+               ]
     no_test = ['password_confirm', 'person']
     crud = ['create']
 
@@ -53,3 +51,4 @@ class TestRegistrationController(ControllerTest):
         for p in ps:
             objectstore.delete(p)
         objectstore.flush()
+        super(TestRegistrationController, self).tearDown()
