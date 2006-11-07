8c6fb028b8c59a9f17d06eb884ab1a835428b599
Jamie Wilkinson <jaq@spacepants.org>
2006-11-07 18:08:13 +1100

    fix john's test suite bugs

--- zookeepr/tests/model/openday/domain/test_registration.py
+++ zookeepr/tests/model/openday/domain/test_registration.py
@@ -1,10 +1,16 @@
 from zookeepr.tests.model import *
 
 class TestOpenday(ModelTest):
-    domain = model.registration.Registration
+    domain = model.openday.Openday
     samples = [dict(fullname='fullname1',
-                    partner_email='partneremail1',
+                    email_address='partneremail1',
                     heardfrom='heardfrom1',
                     heardfromtext='heardfromtext1',
                     opendaydrag=1,
+                    ),
+               dict(fullname='fullname2',
+                    email_address='partneremail2',
+                    heardfrom='heardfrom2',
+                    heardfromtext='heardfewromtext2',
+                    opendaydrag=2,
                     )]
--- zookeepr/tests/model/openday/tables/test_registration.py
+++ zookeepr/tests/model/openday/tables/test_registration.py
@@ -5,15 +5,14 @@ class TestOpendayTable(TableTest):
 
     This table stores registration details.
     """
-    table = model.registration.tables.registration
-    samples = [dict(
-                    fullname='fullname1'
+    table = model.openday.tables.openday
+    samples = [dict(fullname='fullname1',
                     email_address='email1',
                     heardfrom='heardfrom1',
                     heardfromtext='heardfromtext1',
                     opendaydrag=1,
-                dict(
-                    fullname='fullname2'
+                    ),
+               dict(fullname='fullname2',
                     email_address='email2',
                     heardfrom='heardfrom2',
                     heardfromtext='heardfromtext2',
