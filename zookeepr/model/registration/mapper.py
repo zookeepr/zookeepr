0b5c3f4db4147be5bfa03ae072bd63f1c8304ab9
Jamie Wilkinson <jaq@spacepants.org>
2006-11-13 00:02:44 +1100

    update changelog and comments

--- zookeepr/model/registration/mapper.py
+++ zookeepr/model/registration/mapper.py
@@ -12,6 +12,9 @@ mapper(AccommodationOption, accommodation_option,
     }
        )
 
+
+# The following selectable represents the aggregate sum of registrations
+# that have selected a location as their preferred accommodation.
 accommodation_location_beds_taken_selectable = \
     select([accommodation_location.c.id.label('loc_id'),
             func.count(registration.c.id).label('beds_taken')],
@@ -19,7 +22,10 @@ accommodation_location_beds_taken_selectable = \
                                          accommodation_option),
                                registration)],
            group_by=[accommodation_location.c.id]).alias('loc_beds_taken')
-                                             
+
+# This mapper then joins the options onto the location+registration count above
+# so we can determine how many beds are taken for that option.  This is necessary
+# as bed count is per location, not per option.
 mapper(Accommodation,
        select([accommodation_option.c.id,
                accommodation_location.c.name,
