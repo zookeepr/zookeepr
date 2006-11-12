b5e3adbe7cfeb1628848b43974a09626d58cb1dc
Jamie Wilkinson <jaq@spacepants.org>
2006-11-12 23:58:17 +1100

    remove debugging prints

--- zookeepr/controllers/registration.py
+++ zookeepr/controllers/registration.py
@@ -131,13 +131,8 @@ class RegistrationController(BaseController, Create):
 
 
     def new(self):
-        print "calling new"
         as = Query(model.Accommodation).select()
-        print "as", as
-        print "post query"
         c.accommodation_collection = filter(lambda a: a.get_available_beds() >= 1, as)
-        print 'post filter'
-        print "accommodation options are:", c.accommodation_collection
 
         errors = {}
         defaults = dict(request.POST)
