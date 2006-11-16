3841153e88e955c08e6f73b06c5148894faf9b3a
Jamie Wilkinson <jaq@spacepants.org>
2006-11-16 15:14:04 +1100

    fix bugs in attachment test, make it run

--- zookeepr/tests/model/proposal/domain/test_attachment.py
+++ zookeepr/tests/model/proposal/domain/test_attachment.py
@@ -3,7 +3,7 @@ import datetime
 from zookeepr.tests.model import *
 
 class TestAttachmentModel(CRUDModelTest):
-    model = 'proposal.Attachment'
+    domain = model.proposal.Attachment
     samples = [dict(filename='attachment 1',
                     content_type='text/plain',
                     creation_timestamp=datetime.datetime(2006,8,29,16,23,37),
@@ -14,3 +14,19 @@ class TestAttachmentModel(CRUDModelTest):
                     content="some more different text"),
                ]
     mangles = {'content': lambda t: buffer(t)}
+
+    def setUp(self):
+        super(TestAttachmentModel, self).setUp()
+        self.p = model.Proposal(title='a', abstract='b')
+        objectstore.save(self.p)
+        objectstore.flush()
+        self.pid = self.p.id
+
+    def tearDown(self):
+        objectstore.delete(Query(model.Proposal).get(self.pid))
+        objectstore.flush()
+        super(TestAttachmentModel, self).tearDown()
+
+    def additional(self, obj):
+        self.p.attachments.append(obj)
+        return obj
