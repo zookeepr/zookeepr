e2ed81cced20906cc3c1d62eaf626ad5506c0b0f
Jamie Wilkinson <jaq@spacepants.org>
2006-11-16 17:20:22 +1100

    enable crud tests on review model, and fix mapper bug in review->proposal join with doubled-up reference

--- zookeepr/tests/model/proposal/domain/test_review.py
+++ zookeepr/tests/model/proposal/domain/test_review.py
@@ -3,8 +3,52 @@ import sqlalchemy.exceptions
 from zookeepr.tests.model import *
 
 class TestReviewModel(CRUDModelTest):
-#    domain = model.Review
-#    samples = [dict(proposal
+    domain = model.Review
+    samples = [dict(familiarity=1,
+                    technical=1,
+                    experience=1,
+                    coolness=1,
+                    comment="c1",
+                    ),
+                dict(familiarity=2,
+                    technical=2,
+                    experience=2,
+                    coolness=2,
+                    comment="c2",
+                    ),
+                ]
+
+    def setUp(self):
+        super(TestReviewModel, self).setUp()
+
+        self.proposal = model.Proposal(title='p', abstract='a')
+        objectstore.save(self.proposal)
+
+        self.reviewer = model.Person(fullname='snuh', email_address='reviewer@example.org')
+        objectstore.save(self.reviewer)
+
+        self.stream = model.Stream(name="foo")
+        objectstore.save(self.stream)
+
+        objectstore.flush()
+
+        self.pid = self.proposal.id
+        self.rid = self.reviewer.id
+        self.sid = self.stream.id
+
+    def tearDown(self):
+        objectstore.delete(Query(model.Stream).get(self.sid))
+        objectstore.delete(Query(model.Proposal).get(self.pid))
+        objectstore.delete(Query(model.Person).get(self.rid))
+        objectstore.flush()
+
+        super(TestReviewModel, self).tearDown()
+
+    def additional(self, review):
+        review.proposal = self.proposal
+        review.reviewer = self.reviewer
+        review.stream = self.stream
+        return review
 
     def test_one_review_per_reviewer_per_proposal(self):
         """Test that reviewers can only review each proposal once"""
