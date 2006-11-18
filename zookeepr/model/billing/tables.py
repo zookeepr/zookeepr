a7cca44af516661f758a4e65bf119d9aa9b54995
Jamie Wilkinson <jaq@spacepants.org>
2006-11-18 14:57:09 +1100

    add invoice item test, table definition

---
+++ zookeepr/model/billing/tables.py
@@ -0,0 +1,11 @@
+import sqlalchemy.mods.threadlocal
+from sqlalchemy import *
+
+invoice_item = Table('invoice_item',
+                     Column('id', Integer, primary_key=True),
+
+                     Column('description', String,
+                            nullable=False),
+                     Column('cost', Float,
+                            nullable=False),
+                     )
