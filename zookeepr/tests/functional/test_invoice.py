45af49cc67eb7ad081ee01d333559039c5b98685
Jamie Wilkinson <jaq@spacepants.org>
2006-11-21 10:43:22 +1100

    invoice contorller setup sorted

--- zookeepr/tests/functional/test_invoice.py
+++ zookeepr/tests/functional/test_invoice.py
@@ -9,12 +9,15 @@ class TestInvoiceController(SignedInCRUDControllerTest):
         self.invoice = model.Invoice(issue_date=datetime.date(2006,11,21))
         ii1 = model.InvoiceItem(description='line 1', cost=1)
         self.invoice.items.append(ii1)
+        self.person.invoices.append(self.invoice)
         self.dbsession.save(ii1)
         self.dbsession.save(self.invoice)
         self.dbsession.flush()
+        self.iid = self.invoice.id
 
     def tearDown(self):
-        self.dbsession.delete(self.invoice)
+        invoice = self.dbsession.query(model.Invoice).get(self.iid)
+        self.dbsession.delete(invoice)
         self.dbsession.flush()
 
         super(TestInvoiceController, self).tearDown()
