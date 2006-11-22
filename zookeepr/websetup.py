9ca39fa9576ec09e43b417d1ddef4803623ed97f
Jamie Wilkinson <jaq@spacepants.org>
2006-11-23 00:10:59 +1100

    debugging

--- zookeepr/websetup.py
+++ zookeepr/websetup.py
@@ -56,6 +56,10 @@ def setup_config(command, filename, section, vars):
     p.close()
 
     try:
+        if os.path.exists(app_conf['dynamic_html_dir']):
+            print "ok!"
+        else:
+            print "boo"
         os.symlink(os.path.join(app_conf['moin_data'], 'attachments'),
                    os.path.join(app_conf['dynamic_html_dir'], 'att-data'))
     except OSError, e:
