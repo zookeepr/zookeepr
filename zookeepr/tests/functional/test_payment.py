ce1f37691c43a8d0ddca8226ae9d054fadfd2ade
Jamie Wilkinson <jaq@spacepants.org>
2006-11-22 12:30:39 +1100

    add stub test

---
+++ zookeepr/tests/functional/test_payment.py
@@ -0,0 +1,8 @@
+from zookeepr.tests.functional import *
+
+class TestPaymentController(ControllerTest):
+    """Test that when we pump data into the payment, it does the right thing.
+    """
+
+    pass
+    #def test_payment_
