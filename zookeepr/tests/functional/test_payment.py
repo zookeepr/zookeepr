d12ed2d7ed7fb08f19478bad39c8a8a7ec12bb19
Jamie Wilkinson <jaq@spacepants.org>
2006-11-22 20:36:22 +1100

    more testing yaya

--- zookeepr/tests/functional/test_payment.py
+++ zookeepr/tests/functional/test_payment.py
@@ -1,3 +1,6 @@
+import hmac
+import sha
+
 from paste.fixture import Dummy_smtplib
 
 from zookeepr.tests.functional import *
@@ -6,7 +9,71 @@ class TestPaymentController(ControllerTest):
     """Test that when we pump data into the payment, it does the right thing.
     """
 
-#     def test_payment_received(self):
+    def setUp(self):
+        super(TestPaymentController, self).setUp()
+        Dummy_smtplib.install()
+
+        self.params = dict(InvoiceID=1,
+                           AuthNum=5261,
+                           Amount=69001,
+                           RefundKey='KJdW+tSk95x+d54+gHB1',
+                           MerchantID='Test',
+                           Status='Denied',
+                           Settlement=1111,
+                           ErrorString='x',
+                           CardName='y',
+                           RequestedPage='ReceiptPage1',
+                           CardType='VISA',
+                           MAC='0cf459a485261747bb565ec47ec434ee6fdbasdf',
+                           CardNumber='51000...0009',
+                           PaymentID=201,
+                           TransID='SimProxy 54021550',
+                           ORIGINAL_AMOUNT=69001,
+                           Surcharge=1)
+
+    def tearDown(self):
+        if Dummy_smtplib.existing:
+            Dummy_smtplib.existing.reset()
+
+        self.dbsession.delete(self.dbsession.query(model.PaymentReceived).select()[0])
+        self.dbsession.flush()
+
+        super(TestPaymentController, self).tearDown()
+
+    def test_no_payment(self):
+        # a random hit on the page
+        
+        resp = self.app.get('/payment/new', params=self.params)
+
+        print resp
+        
+        self.failUnless("Invalid HMAC" in  Dummy_smtplib.existing.message)
+
+        self.assertEqual('/Errors/InvalidPayment', resp.header('location'))
+
+    def gen_mac(self, fields):
+        """Generate a HMAC for the fields"""
+        keys = fields.keys()
+        keys.sort()
+        string_to_mac = '&'.join(['%s=%s' % (key, fields[key]) for key in keys if key != 'MAC'])
+        mac = hmac.new('foo', string_to_mac, sha).hexdigest()
+        fields['MAC'] = mac
+        return fields
+    
+    def test_valid_hmac_no_payment(self):
+        # form has a valid HMAC but nonexistent payment
+
+        resp = self.app.get('/payment/new', params=self.gen_mac(self.params))
+
+        print resp
+
+        print Dummy_smtplib.existing.message
+
+        self.failUnless("Nonexistent Payment" in Dummy_smtplib.existing.message)
+        self.assertEqual("/Errors/MissingPayment", resp.header('location'))
+
+
+#     def test_valid_payment_wrong_invoice(self):
 #         invoice = model.Invoice()
 #         self.dbsession.save(invoice)
 #         ii = model.InvoiceItem('foo', 1, 100)
@@ -15,38 +82,8 @@ class TestPaymentController(ControllerTest):
 #         invoice.items.append(ii)
 
 #         # do stuff
-
-    def setUp(self):
-        super(TestPaymentController, self).setUp()
-        Dummy_smtplib.install()
-
-    def tearDown(self):
-        if Dummy_smtplib.existing:
-            Dummy_smtplib.existing.reset()
-        super(TestPaymentController, self).tearDown()
-
-    def test_no_payment(self):
-        # a random hit on the page
         
-        params = dict(InvoiceID=1,
-                      AuthNum=5261,
-                      Amount=69001,
-                      RefundKey='KJdW+tSk95x+d54+gHB1',
-                      MerchantID='Test',
-                      Status='Denied',
-                      Settlement=1111,
-                      ErrorString='x',
-                      CardName='y',
-                      RequestedPage='ReceiptPage1',
-                      CardType='VISA',
-                      MAC='0cf459a485261747bb565ec47ec434ee6fdbasdf',
-                      CardNumber='51000...0009',
-                      PaymentID=201,
-                      TransID='SimProxy 54021550',
-                      ORIGINAL_AMOUNT=69001,
-                      Surcharge=1)
-        resp = self.app.get('/payment/new', params=params)
-
-        self.failUnless("Invalid HMAC" in  Dummy_smtplib.existing.message)
-
-        self.assertEqual('/Errors/InvalidPayment', resp.header('location'))
+#         self.fail("not really")
+        
+        
+        
