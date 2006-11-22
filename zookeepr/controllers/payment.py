62be3914fe3d119a615ac0e9b6c95854a937168c
Jamie Wilkinson <jaq@spacepants.org>
2006-11-22 12:30:08 +1100

    put comments in

--- zookeepr/controllers/payment.py
+++ zookeepr/controllers/payment.py
@@ -11,30 +11,32 @@ class PaymentController(BaseController, Create):
     individual = 'payment'
 
     def _new_postflush():
-        """
-   - Verify HMAC (already there)
-   - Check the payment ID exists and load it
-   - Compare loaded to received to make sure the following match
-      * invoiceid
-      * amount, orignal_amount
+        payment = c.payment
+        #         """
+        #    - Verify HMAC (already there)
+        c.payment
+        #    - Check the payment ID exists and load it
+        #    - Compare loaded to received to make sure the following match
+        #       * invoiceid
+        #       * amount, orignal_amount
 
-* If something doesn't match then
-    * set payments_received.commsecure_status == SHENANIGANS and email
-seven-contact
-    * set payments.status = FAILED_SHENANIGANS
-    * Tell the user something wierd happened and to email seven-contact
+# * If something doesn't match then
+#     * set payments_received.commsecure_status == SHENANIGANS and email
+# seven-contact
+#     * set payments.status = FAILED_SHENANIGANS
+#     * Tell the user something wierd happened and to email seven-contact
 
-* If everything matches
-    * set payments_received.commsecure_status == GOOD and email seven-contact
+# * If everything matches
+#     * set payments_received.commsecure_status == GOOD and email seven-contact
 
-* If payments_received.status == Declined
-    * set payments.status = Declined
-    * set payments.payments_received_id = payments_received.id
-    * Send the user payments_received.ErrorString
+# * If payments_received.status == Declined
+#     * set payments.status = Declined
+#     * set payments.payments_received_id = payments_received.id
+#     * Send the user payments_received.ErrorString
 
-* If payments_received.status == Accepted
-   * set payments.status = Accepted
-   * set payments.payments_received_id = payments_received.id
-   * set invoice.status = PAID
-   * tell the user ALL GOOD
-   """
+# * If payments_received.status == Accepted
+#    * set payments.status = Accepted
+#    * set payments.payments_received_id = payments_received.id
+#    * set invoice.status = PAID
+#    * tell the user ALL GOOD
+#    """
