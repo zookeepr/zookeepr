8f8cef5e5ab321311fc54fc281d670f2ef032c5d
John Ferlito <johnf@inodes.org>
2006-11-23 19:16:47 +1100

    Stop dupe payments

--- zookeepr/controllers/invoice.py
+++ zookeepr/controllers/invoice.py
@@ -20,8 +20,8 @@ class InvoiceController(SecureController, Read):
 
         This method bounces the user off to the commsecure website.
         """
-        if invoice.person.invoices:
-            if invoice.person.invoices[0].good_payments or invoice.person.invoices[0].bad_payments:
+        if c.invoice.person.invoices:
+            if c.invoice.person.invoices[0].good_payments or c.invoice.person.invoices[0].bad_payments:
                 redirect_to("/Errors/InvoiceAlreadyPaid")
 
         # get our merchant id and secret
