da3ddda2eaa8102c284d665d03a4d74aac955615
John Ferlito <johnf@inodes.org>
2006-11-28 10:53:33 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zookeepr',
-    version="0.3.3.3",
+    version="0.3.4",
     #description="",
     #author="",
     #author_email="",
