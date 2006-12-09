c4510fa35cc28b8294b37868e53c4628895700f7
John Ferlito <johnf@inodes.org>
2006-12-09 13:28:09 +1100

    View profile list

--- zookeepr/controllers/profile.py
+++ zookeepr/controllers/profile.py
@@ -1,13 +1,20 @@
 from zookeepr.lib.base import *
-from zookeepr.lib.crud import Read, Update
+from zookeepr.lib.crud import Read, Update, List
+from zookeepr.lib.auth import AuthRole
 from zookeepr import model
 
-class ProfileController(BaseController, Read, Update):
+class ProfileController(BaseController, Read, Update, List):
     model = model.Person
     individual = 'profile'
 
     def index(self):
-        if not 'signed_in_person_id' in session:
+        r = AuthRole('organiser')
+        if 'signed_in_person_id' in session:
+            c.signed_in_person = self.dbsession.get(model.Person, session['signed_in_person_id'])
+            if not r.authorise(self):
+                redirect_to(action='view', id=session['signed_in_person_id'])
+        else:
             abort(403)
 
-        redirect_to(action='view', id=session['signed_in_person_id'])
+        return super(ProfileController, self).index()
+
