5587bd6eae346b688ab4ada282a4dae4d29e3f6b
Jiri Baum <jiri@baum.com.au>
2007-03-05 20:37:35 +1100

    fix the profile/nnn information leak

--- zookeepr/controllers/profile.py
+++ zookeepr/controllers/profile.py
@@ -1,13 +1,20 @@
+from zookeepr.lib.auth import SecureController, AuthFunc
 from zookeepr.lib.base import *
 from zookeepr.lib.crud import Read, Update
 from zookeepr import model
 
-class ProfileController(BaseController, Read, Update):
+class ProfileController(SecureController, Read, Update):
     model = model.Person
     individual = 'profile'
+
+    permissions = {'view': [AuthFunc('is_same_id')]}
 
     def index(self):
         if not 'signed_in_person_id' in session:
             abort(403)
 
         redirect_to(action='view', id=session['signed_in_person_id'])
+
+    def is_same_id(self, *args):
+        return self.obj.id == session['signed_in_person_id']
+
