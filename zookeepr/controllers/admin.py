cd2761c2dc818644139ac30caebe7d22a4920de9
Jiri Baum <jiri@baum.com.au>
2007-04-12 04:02:11 +1000

    Helper functions in the admin controller for directly querying the database, bypassing all the MVC stuff.
    Ideally, one would never use them.
    
    In practice, they may end up a half-way house for the scripts currently in
    bin/database (if we're rushed later) and other similar scripts as needed.

--- zookeepr/controllers/admin.py
+++ zookeepr/controllers/admin.py
@@ -11,3 +11,29 @@ class AdminController(SecureController):
 	Testing, testing, 1, 2, 3.
         """
         return Response("This is a test. Hope you've studied!")
+
+def sql_response(sql):
+    """ This function bypasses all the MVC stuff and just puts up a table
+    of results from the given SQL statement.
+
+    Ideally, of course, it should never be used.
+
+    Example:
+	def foo(self):
+	    return sql_response('select * from person')
+    """
+    import zookeepr.model
+    res = zookeepr.model.metadata.get_engine().execute(sql);
+    c.columns = res.keys
+    c.data = res.fetchall()
+    c.sql = sql
+    return render_response('admin/sqltable.myt')
+    
+def sql_data(sql):
+    """ This function bypasses all the MVC stuff and just gives you a
+    two-dimensional array based on the given SQL statement.
+
+    Ideally, of course, it should never be used.
+    """
+    import zookeepr.model
+    return zookeepr.model.metadata.get_engine().execute(sql).fetchall();
---
+++ zookeepr/templates/admin/sqltable.myt
@@ -0,0 +1,19 @@
+<table>
+<tr>
+% for header in c.columns:
+  <th><% header %></th>
+% # endfor
+</tr>
+
+% for row in c.data:
+  <tr>
+%   for item in row:
+    <td><% item %></td>
+%   # endfor
+  </tr>
+% # endfor
+</table>
+
+<pre>
+<% c.sql %>
+</pre>
