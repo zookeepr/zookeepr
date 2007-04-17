2b846d0f185ebe3af367c7196af4e1933d9b2d2d
Jiri Baum <jiri@baum.com.au>
2007-04-17 22:03:39 +1000

    Make unicode conversions default to utf8 rather than ascii.

---
+++ sitecustomize.py
@@ -0,0 +1,4 @@
+# This file is imported by the built-in site.py file. Normally one would
+# ignore it, but it's the last time setdefaultencoding() can be called...
+import sys
+sys.setdefaultencoding('utf8')
