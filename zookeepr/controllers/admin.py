2358b1d32cbd7bdf88d1b7d4b02508501adb794d
Jiri Baum <jiri@baum.com.au>
2007-05-22 17:07:30 +1000

    order admin/list_miniconfs by title

--- zookeepr/controllers/admin.py
+++ zookeepr/controllers/admin.py
@@ -44,7 +44,10 @@ class AdminController(SecureController):
 
     def list_miniconfs(self):
         """ List of miniconfs """
-        return sql_response("""select proposal.id as id, title, abstract, url, firstname || ' ' || lastname as name from proposal, person, person_proposal_map where proposal_type_id = 2 and person.id=person_id and proposal.id=proposal_id""")
+        return sql_response("""select proposal.id as id, title, abstract,
+	url, firstname || ' ' || lastname as name from proposal, person,
+	person_proposal_map where proposal_type_id = 2 and
+	person.id=person_id and proposal.id=proposal_id order by title""")
     def list_attachments(self):
         """ List of attachments """
         return sql_response('''
