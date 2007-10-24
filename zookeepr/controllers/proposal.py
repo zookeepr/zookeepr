bee3d6caf2dc12d8b1f0354d8782ac4cfbd053a6
Jiri Baum <jiri@baum.com.au>
2007-10-25 02:40:00 +1000

    -

--- zookeepr/controllers/proposal.py
+++ zookeepr/controllers/proposal.py
@@ -71,6 +71,7 @@ class ProposalController(SecureController, View, Modify):
                    "delete": [AuthFunc('is_submitter')],
                    "index": [AuthRole('reviewer'), AuthRole('organiser')],
                    }
+    anon_actions = ['talk']
 
     def __before__(self, **kwargs):
         super(ProposalController, self).__before__(**kwargs)
