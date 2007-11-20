c9b855ec347642ef531cba73f4b7ebee154e5c60
Jiri Baum <jiri@baum.com.au>
2007-11-20 22:01:54 +1100

    -

--- zookeepr/controllers/discount_code.py
+++ zookeepr/controllers/discount_code.py
@@ -30,7 +30,7 @@ class ExistingPersonValidator(validators.FancyValidator):
 
 
 class DiscountCodeSchema(BaseSchema):
-    count = BoundedInt(min=1, max=100)
+    count = BoundedInt(min=0, max=100)
     leader_id = BoundedInt()
     code = validators.String()
     type = validators.String(not_empty=True)
