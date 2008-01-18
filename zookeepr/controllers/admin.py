acf19bbd82a93f8cb36ceac8fe298e131dd9df07
Jiri Baum <jiri@baum.com.au>
2008-01-18 13:29:08 +1100

    -

--- zookeepr/controllers/admin.py
+++ zookeepr/controllers/admin.py
@@ -1308,6 +1308,8 @@ class AdminController(SecureController):
 	    type = type_map.get(type, type).lower()
 	    if p.is_speaker():
 	        type = 'speaker'
+	    elif 'team' in [rl.name for rl in r.person.roles]):
+	        type = 'organiser'
 	    if r.type in keynote_types:
 	        nka = 'k'
             else:
