e11bc3c5c3843c64a91722b3912d90a0b5aa0ca6
John Ferlito <johnf@inodes.org>
2008-05-24 15:28:10 +1000

    whitespace cleanups

--- zookeepr/lib/mail.py
+++ zookeepr/lib/mail.py
@@ -4,12 +4,12 @@ from zookeepr.config.lca_info import lca_info
 
 def email(recipients, body):
     try:
-	s = smtplib.SMTP(
-	    request.environ['paste.config']['global_conf'].get('smtp_server'))
-	s.sendmail(
+        s = smtplib.SMTP(
+                request.environ['paste.config']['global_conf'].get('smtp_server'))
+        s.sendmail(
             lca_info['contact_email'],
             recipients, body)
-	s.quit()
+        s.quit()
     except:
-	pass
+        pass
 
