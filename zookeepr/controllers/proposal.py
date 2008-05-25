f699ae2ea72a6e85a58a0f70e553c8bd11135834
Joshua Hesketh <josh@nitrotech.org>
2008-05-25 17:26:46 +1000

    -

--- zookeepr/controllers/proposal.py
+++ zookeepr/controllers/proposal.py
@@ -66,7 +66,7 @@ class ProposalController(SecureController, View, Modify):
     permissions = {"new": [AuthFalse()],
                    "edit": [AuthFunc('is_submitter'), AuthRole('organiser')],
                    "view": [AuthFunc('is_submitter'), AuthRole('reviewer'),
-						        AuthRole('organiser')],
+                                                        AuthRole('organiser')],
                    "summary": [AuthRole('organiser'), AuthRole('reviewer')],
                    "delete": [AuthFunc('is_submitter')],
                    "index": [AuthRole('reviewer'), AuthRole('organiser')],
@@ -118,32 +118,32 @@ class ProposalController(SecureController, View, Modify):
         errors = {}
 
         # Next ID for skipping
-        collection = self.dbsession.from_statement("
-SELECT
-    *
-FROM
-	review AS r
-LEFT JOIN
-	proposal AS p
-		ON(p.id=r.proposal_id)
-WHERE	
-	p.proposal_type_id IN(1,3)
-GROUP BY
-	r.proposal_id
-HAVING COUNT(r.proposal_id) < (
-	(SELECT COUNT(id) FROM review) /
-	(SELECT COUNT(id) FROM proposal WHERE proposal_type_id IN(1,3)) + 1)
-ORDER BY
-	RANDOM()")
+        collection = self.dbsession.from_statement("""
+                            SELECT
+                                *
+                            FROM
+                                    review AS r
+                            LEFT JOIN
+                                    proposal AS p
+                                            ON(p.id=r.proposal_id)
+                            WHERE        
+                                    p.proposal_type_id IN(1,3)
+                            GROUP BY
+                                    r.proposal_id
+                            HAVING COUNT(r.proposal_id) < (
+                                    (SELECT COUNT(id) FROM review) /
+                                    (SELECT COUNT(id) FROM proposal WHERE proposal_type_id IN(1,3)) + 1)
+                            ORDER BY
+                                    RANDOM()""")
         for proposal in collection:
             #print proposal.id
             if not [ r for r in proposal.reviews if r.reviewer == c.signed_in_person ] and proposal.id != id:
                 c.next_review_id = proposal.id
                 break
-	else:
-	    # somehow didn't find one, so pick one at random
-	    # (the collection is shuffled, so item 0 is random enough)
-	    c.next_review_id = 1
+        else:
+            # somehow didn't find one, so pick one at random
+            # (the collection is shuffled, so item 0 is random enough)
+            c.next_review_id = 1
 
 
 
@@ -204,14 +204,14 @@ ORDER BY
 
     def talk(self, id):
         if c.proposal.accepted:
-	    return render_response('proposal/talk.myt')
-	else:
-	    return redirect_to('/programme')
+            return render_response('proposal/talk.myt')
+        else:
+            return redirect_to('/programme')
 
     def edit(self, id):
         c.person = c.signed_in_person # FIXME - is this correct? --Jiri 27.4.2008
-	c.cfptypes = self.dbsession.query(ProposalType).all()
-	c.tatypes = self.dbsession.query(AssistanceType).all()
+        c.cfptypes = self.dbsession.query(ProposalType).all()
+        c.tatypes = self.dbsession.query(AssistanceType).all()
 
         errors = {}
         defaults = dict(request.POST)
@@ -247,7 +247,7 @@ ORDER BY
     def index(self):
         c.person = c.signed_in_person
         # hack for bug#34, don't show miniconfs to reviewers
-	# Jiri: unless they're also organisers...
+        # Jiri: unless they're also organisers...
         if 'organiser' in [r.name for r in c.signed_in_person.roles]:
             c.proposal_types = self.dbsession.query(ProposalType).all()
         else:
@@ -296,7 +296,7 @@ ORDER BY
         total_score = 0
         num_reviewers = 0
         for review in proposal.reviews:
-	    if review.score is not None:
+            if review.score is not None:
                 num_reviewers += 1
                 total_score += review.score
         if num_reviewers == 0:
