75c7a0f389ebd09d41c4b65bddcf00e35ab9de94
Joshua Hesketh <josh@nitrotech.org>
2008-06-21 22:58:32 +1000

    Attachment delete permissions fixed

--- zookeepr/controllers/attachment.py
+++ zookeepr/controllers/attachment.py
@@ -1,20 +1,35 @@
 from zookeepr.lib.auth import SecureController, AuthRole, AuthTrue, AuthFunc
 from zookeepr.lib.base import *
 from zookeepr.lib.crud import Delete
-from zookeepr.model import Attachment
+from zookeepr.model import Attachment, Proposal
 
 class AttachmentController(SecureController, Delete):
     model = Attachment
     individual = 'attachment'
-    redirect_map = {'delete': dict(controller='proposal', action='view', id=session.get('proposal_id', 1))}
 
     permissions = {
       'view': True,
       'delete': [AuthFunc('is_submitter'), AuthRole('organiser')]
     }
 
+    def delete(self, id):
+        if request.method == 'POST' and self.obj is not None:
+            self.dbsession.delete(self.obj)
+            self.dbsession.flush()
+
+            proposal = self.dbsession.query(model.Proposal).get(self.obj.proposal_id)
+            redirect = dict(controller='proposal', action='view', id=session.get('proposal_id', proposal.id))
+            self.redirect_to('delete', redirect)
+
+        # call the template
+        return render_response('%s/confirm_delete.myt' % self.individual)
+
     def is_submitter(self):
-        return True
+        proposal = self.dbsession.query(model.Proposal).get(self.obj.proposal_id)
+        if c.signed_in_person in proposal.people:
+            return True
+        else:
+            return False
 
     def view(self, id):
         att = self.dbsession.query(model.Attachment).get(id)
