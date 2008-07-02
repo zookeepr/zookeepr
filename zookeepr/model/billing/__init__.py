05fdd8fb21424e43a4f8f7eb99593c5e66bd0ffd
James Iseppi <james@iseppi.org>
2008-07-03 09:53:42 +1000

    * import ProductCategory

--- zookeepr/model/billing/__init__.py
+++ zookeepr/model/billing/__init__.py
@@ -1,1 +1,1 @@
-from domain import Ceiling, Product, InvoiceItem, Invoice, PaymentReceived, Payment, VoucherCode
+from domain import Ceiling, ProductCategory, Product, InvoiceItem, Invoice, PaymentReceived, Payment, VoucherCode
