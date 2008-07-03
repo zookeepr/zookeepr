eb0d0714a125a106a3747a019dbaa5d34f2626e8
James Iseppi <james@iseppi.org>
2008-07-03 13:17:49 +1000

    * Remove the pretty much non-existant Accomodation controller

--- zookeepr/controllers/accommodation.py
+++
@@ -1,9 +0,0 @@
-from zookeepr.lib.auth import SecureController, AuthRole
-from zookeepr.lib.base import *
-from zookeepr.lib.crud import List
-
-class AccommodationController(SecureController, List):
-    individual = 'accommodation'
-    model = model.Accommodation
-    permissions = {'index': [AuthRole('site-admin')],
-                   }
