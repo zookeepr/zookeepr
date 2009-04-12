5ab462c8589f8966033a016db93c908611f7f560
James Iseppi <james@iseppi.org>
2009-04-12 13:32:47 +1000

    * We had the person_proposal_map in 2 files for some reason. Now Fixed

--- zookeepr/model/person_proposal_map.py
+++ zookeepr/model/person_proposal_map.py
@@ -6,7 +6,8 @@ from zookeepr.model.meta import metadata
 def setup(meta):
     pass
 
+# for doing n-n mappings of people and proposals
 person_proposal_map = sa.Table('person_proposal_map', metadata,
-        sa.Column('person_id', sa.types.Integer, sa.ForeignKey('person.id')),
-        sa.Column('proposal_id',   sa.types.Integer, sa.ForeignKey('proposal.id'))
+        sa.Column('person_id', sa.types.Integer, sa.ForeignKey('person.id'), nullable=False),
+        sa.Column('proposal_id',   sa.types.Integer, sa.ForeignKey('proposal.id'), nullable=False)
 )
