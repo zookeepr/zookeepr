3631dec13d497a76597858ec5cc4a1452955795d
Francois Marier <fmarier@gmail.com>
2009-05-18 23:31:43 +1200

    Attachment: test/fix delete and view actions

--- zookeepr/controllers/attachment.py
+++ zookeepr/controllers/attachment.py
@@ -32,45 +32,59 @@ class AttachmentController(BaseController):
     @dispatch_on(POST="_delete")
     def delete(self, id):
         c.attachment = Attachment.find_by_id(id)
-        author_id = c.attachment.proposal.person.id
+        c.proposal = Proposal.find_by_id(c.attachment.proposal_id)
+        
+        if not h.auth.has_organiser_role:
+            authorized = False
+            for person in c.proposal.people:
+                if h.auth.is_same_zookeepr_user(person.id):
+                    authorized = True
+                    break
+            if not authorized:
+                # Raise a no_auth error
+                h.auth.no_role()
 
-        # We need to recheck auth in here so we can pass in the id
-        if not h.auth.authorized(h.auth.Or(h.auth.is_same_zookeepr_user(author_id), h.auth.has_organiser_role)):
-            # Raise a no_auth error
-            h.auth.no_role()
-        return render_response('/attachment/confirm_delete.mako')
+        return render('/attachment/confirm_delete.mako')
 
     @validate(schema=None, form='delete', post_only=True, on_get=True, variable_decode=True)
     def _delete(self, id):
-        c.attachment = Attachment.find_by_id(id)
-        author_id = c.attachment.proposal.person.id
+        attachment = Attachment.find_by_id(id)
+        proposal = Proposal.find_by_id(c.attachment.proposal_id)
 
-        if not h.auth.authorized(h.auth.Or(h.auth.is_same_zookeepr_user(author_id), h.auth.has_organiser_role)):
-            # Raise a no_auth error
-            h.auth.no_role()
+        if not h.auth.has_organiser_role:
+            authorized = False
+            for person in proposal.people:
+                if h.auth.is_same_zookeepr_user(person.id):
+                    authorized = True
+                    break
+            if not authorized:
+                # Raise a no_auth error
+                h.auth.no_role()
 
-        proposal_id = c.attachment.proposal.id
         meta.Session.delete(c.attachment)
         meta.Session.commit()
 
         h.flash("Attachment Deleted")
-        redirect_to(controller='proposal', action='view', id=proposal_id)
+        redirect_to(controller='proposal', action='view', id=proposal.id)
 
     def view(self, id):
         attachment = Attachment.find_by_id(id)
-        author_id = attachment.proposal.person.id
+        proposal = Proposal.find_by_id(attachment.proposal_id)
 
-        if not h.auth.authorized(h.auth.Or(h.auth.is_same_zookeepr_user(author_id), h.auth.has_organiser_role)):
-            # Raise a no_auth error
-            h.auth.no_role()
+        if not h.auth.has_organiser_role:
+            authorized = False
+            for person in proposal.people:
+                if h.auth.is_same_zookeepr_user(person.id):
+                    authorized = True
+                    break
+            if not authorized:
+                # Raise a no_auth error
+                h.auth.no_role()
 
-        response = Response(attachment.content)
         response.headers['content-type'] = attachment.content_type
         response.headers.add('content-transfer-encoding', 'binary')
         response.headers.add('content-length', len(attachment.content))
         response.headers['content-disposition'] = 'attachment; filename="%s";' % attachment.filename
-
         response.headers.add('Pragma', 'cache')
         response.headers.add('Cache-Control', 'max-age=3600,public')
-
-        return response
+        return attachment.content
--- zookeepr/templates/attachment/confirm_delete.mako
+++ zookeepr/templates/attachment/confirm_delete.mako
@@ -6,6 +6,7 @@ ${ h.form(h.url_for()) }
 
 <p>Are you sure you want to delete this attachment?</p>
 
-<p>${ h.submit('submit', 'Yes, delete') }</p>
+<p>${ h.submit('submit', 'Yes, delete') }
+or ${ h.link_to('No, take me back.', url=h.url_for(controller='proposal', action='view', id=c.proposal.id)) }</p>
 
 ${ h.end_form() }
