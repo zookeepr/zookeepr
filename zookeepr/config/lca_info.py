ddbe0f385cb863edc0bbd59f3a034e643f136ed7
Andrew Ruthven <andrew@etc.gen.nz>
2009-07-15 09:53:20 +1000

    Fix the name of the Creative commons license.

--- zookeepr/config/lca_info.py
+++ zookeepr/config/lca_info.py
@@ -30,7 +30,7 @@ lca_info = {
   'event_phone_number': '+64 4 802 0422',
   'event_byline': 'linux.conf.au 2010 | 18 - 23 Jan | Follow the signs!',
   'date' : datetime(2010, 1, 17, 9, 0, 00),
-  'media_license_name' : 'Creative Commons ShareAlike License',
+  'media_license_name' : 'Creative Commons Attribution-Share Alike License',
   'media_license_url'  : 'http://creativecommons.org/licenses/by-sa/3.0/',
 
   'invoice_message' : 'To qualify for the earlybird discount you must have registered and paid by the 27th of October (unless earlybird tickets sell out earlier).',
