dd22334ad711daaac8f3e9d5fdf14b53f9252ebc
Andrew Ruthven <andrew@etc.gen.nz>
2009-07-27 11:51:07 +1000

    Make the Accommodation page live, under Wellington for now.

--- zookeepr/config/lca_info.py
+++ zookeepr/config/lca_info.py
@@ -151,7 +151,8 @@ lca_menu = [
 
 lca_submenus = {
   'about': ['linux.conf.au', 'Capital Cabal', 'Venue', 'History', 'New Zealand', 'Linux/Open Source'],
-  'wellington': ['About', 'Sightseeing', 'Pre and Post' ],
+  #'wellington': ['About', 'Sightseeing', 'Pre and Post' ],
+  'wellington': ['About', 'Accommodation', 'Sightseeing', 'Pre and Post' ],
   'sponsors': ['Sponsors', 'Why Sponsor'],
   #'programme': ['About', 'Social Events', 'Open Day', 'Partners Programme'], # stage 0
   #'programme': ['About', 'Keynotes', 'Miniconf Info', 'Presenter FAQ', 'Social Events', 'Open Day', 'Partners Programme'], # stage 1
