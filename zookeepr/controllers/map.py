7199708bea858039bbc83e05ff3c7ca487d5b3e8
Andrew Ruthven <andrew@etc.gen.nz>
2009-10-07 11:51:06 +1300

    Actually add the map controller.

---
+++ zookeepr/controllers/map.py
@@ -0,0 +1,20 @@
+import logging
+import zookeepr.lib.helpers as h
+from pylons import request, response, session, tmpl_context as c
+from pylons.controllers.util import abort, redirect_to
+
+from zookeepr.model import meta
+from zookeepr.model.db_content import DbContent, DbContentType
+
+from zookeepr.lib.base import BaseController, render
+
+log = logging.getLogger(__name__)
+
+
+class MapController(BaseController):
+
+    def index(self):
+        """A Google map on website.
+        """
+
+        return render('map/view.mako')
