7448b40d1f189d7289154e53c34b09e4a17dd197
Andrew Ruthven <andrew@etc.gen.nz>
2010-01-29 10:32:28 +1300

    Add the ability to put captions on images in the slideshow.
    First cut at patch supplied by Glynn Foster, tweaks applied by me.

--- zookeepr/lib/helpers.py
+++ zookeepr/lib/helpers.py
@@ -228,31 +228,46 @@ def slideshow(set, small=None):
         output = "<div class=\"slideshow\" id=\"%s\" style=\"width: %dpx\">" % (set, int(Image.open(small).size[0]))
         small = os.path.basename(small)
 
+        # Optionally load up some captions for the images.
+        caption = dict()
+        caption_file = file_paths['public_path'] + "/images/" + set + "/captions"
+        if os.path.exists(caption_file):
+          file = open(caption_file, 'r')
+          captions = file.readlines()
+
+          # Assign captions to a lookup table
+          for cap in captions:
+             str = cap.partition(':')
+             caption[str[0]] = str[2]
+    
         # Load up all the images in the set directory.
         files = glob(file_paths['public_path'] + "/images/" + set + '/*')
         for file in files:
-           if os.path.isfile(file):
-               short_file = os.path.basename(file)
-               output += "<a href=\"" + file_paths["public_html"] + "/images/" + set + "/" + short_file + "\" rel=\"lightbox[" + set + "]\">"
+            if os.path.isfile(file):
+                short_file = os.path.basename(file)
+                if file == 'captions':
+                    continue
 
-               # If we're looking at the small one we've picked, display
-               # it as well.
-               if short_file == small:
-                   output += "<img src=\"" +  file_paths["public_html"] + "/images/" + set + "/small/" + short_file + "\">"
+                output += "<a href=\"" + file_paths["public_html"] + "/images/" + set + "/" + short_file + "\" rel=\"lightbox[" + set + "]\""
+                if short_file in caption:
+                    output += " title=\"" + caption[short_file] + "\""
+                output += ">"
 
-                   # If there are more than one image in the slideshow
-                   # then also display "more...".
-                   if files.__len__() > 1:
-                      output += '<div class="more">More images...</div>'
-               output += "</a>\n";
+                # If we're looking at the small one we've picked, display
+                # it as well.
+                if short_file == small:
+                    output += "<img src=\"" +  file_paths["public_html"] + "/images/" + set + "/small/" + short_file + "\">"
+    
+                    # If there are more than one image in the slideshow
+                    # then also display "more...".
+                    if files.__len__() > 1:
+                       output += '<div class="more">More images...</div>'
+                output += "</a>\n";
         output += "</div>\n"
         return output
-            
+               
     except IndexError:
         return "no images found"
-
-
-
 
 
 break_re = re.compile(r'(\n|\r\n)(?!\s*<(li|ul|ol)>)')
