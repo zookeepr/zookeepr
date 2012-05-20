12ab9e373d6a7940bdc86f323929625f239d8e4c
James Iseppi <james@iseppi.org>
2012-05-20 14:55:45 +1000

    * Redirect review/edit to proposal/review

--- zookeepr/controllers/review.py
+++ zookeepr/controllers/review.py
@@ -35,30 +35,9 @@ class ReviewController(BaseController):
 
     @dispatch_on(POST="_edit") 
     def edit(self, id):
-        c.form = 'edit'
         c.review = Review.find_by_id(id)
-        self._is_reviewer()
 
-        c.proposal = c.review.proposal
-        defaults = h.object_to_defaults(c.review, 'review')
-
-        c.signed_in_person = h.signed_in_person()
-        form = render('/review/edit.mako')
-        return htmlfill.render(form, defaults)
-
-    @validate(schema=EditReviewSchema(), form='edit', post_only=True, on_get=True, variable_decode=True)
-    def _edit(self, id):
-        c.review = Review.find_by_id(id)
-        self._is_reviewer()
-
-        for key in self.form_result['review']:
-            setattr(c.review, key, self.form_result['review'][key])
-
-        # update the objects with the validated form data
-        meta.Session.commit()
-
-        h.flash("Review has been edited!")
-        redirect_to(action='view', id=id)
+        redirect_to(h.url_for(controller='proposal', id=c.review.proposal.id, action='edit'))
 
     @dispatch_on(POST="_delete")
     def delete(self, id):
