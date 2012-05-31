b51ac20356ee565f4b959e8c35315af0a2648ae8
James Iseppi <james@iseppi.org>
2012-05-31 22:30:57 +1000

    * Delete duplicate files from lca2013 theme

--- zookeepr/themes/lca2013/public/silly.js
+++
@@ -1,7 +0,0 @@
-function toggle_select_hidden(select, field) {
-  if ( document.getElementById(select).value == 'other' ) {
-    document.getElementById(field).style.display = 'inline';
-  } else {
-    document.getElementById(field).style.display = 'none';
-  }
-}
