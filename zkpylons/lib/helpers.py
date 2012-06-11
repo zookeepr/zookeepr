7d571e72a59c725b8aed0421ebfcaaec7b5c6154
Neill Cox <neill.cox@ingenious.com.au>
2012-06-11 14:37:50 +1000

    Remove commented out code from lib/helpers.py

--- zkpylons/lib/helpers.py
+++ zkpylons/lib/helpers.py
@@ -100,64 +100,6 @@ def cycle(*args, **kargs):
         cycle = cycles[name] = iterdict(items)
     return cycle['iter'].next()
 
-#def counter(*args, **kwargs):
-#    """Return the next cardinal in a sequence.
-#
-#    Every time ``counter`` is called, the value returned will be the next
-#    counting number in that sequence.  This is reset to ``start`` on every
-#    request, but can also be reset by calling ``reset_counter()``.
-#
-#    You can optionally specify the number you want to start at by passing
-#    in the ``start`` argument (defaults to 1).
-#
-#    You can also optionally specify the step size you want by passing in
-#    the ``step`` argument (defaults to 1).
-#
-#    Sequences will increase monotonically by ``step`` each time it is
-#    called, until the heat death of the universe or python explodes.
-#
-#    This can be used to count rows in a table::
-#
-#        # In Myghty
-#        % for item in items:
-#        <tr>
-#            <td><% h.counter() %></td>
-#        </tr>
-#        % #endfor
-#
-#    You can used named counters to prevent clashes in nested loops.
-#    You'll have to reset the inner cycle manually though.  See the
-#    documentation for ``webhelpers.text.cycle()`` for a similar
-#    example.
-#    """
-#    # optional name of this list
-#    name = kwargs.get('name', 'default')
-#    # optional starting value for this sequence
-#    start = kwargs.get('start', 1)
-#    # optional step size of this sequence
-#    step = kwargs.get('step', 1)
-#
-#    counters = request_config().environ.setdefault('railshelpers.counters', {})
-#
-#    # ripped off of itertools.count
-#    def do_counter(start, step):
-#        while True:
-#            yield start
-#            start += step
-#
-#    counter = counters.setdefault(name, do_counter(start, step))
-#
-#    return counter.next()
-#
-#def reset_counter(name='default'):
-#    """Resets a counter.
-#
-#    Resets the counter so that it starts from the ``start`` cardinal in
-#    the sequence next time it is used.
-#    """
-#    del request_config().environ['railshelpers.counters'][name]
-#
-
 def webmaster_email(text=None):
     """ E-mail link for the conference contact.
 
@@ -202,35 +144,7 @@ def event_shortname():
     """
     return lca_info['event_shortname']
 
-#def get_temperature():
-#    """ Fetch temperature from the BOM website.
-#
-#    This *REALLY* need to implement some sort of caching mechanism. Sadly I know no
-#    python, so someone else is going to have to write it.
-#    """
-#    return urllib.urlopen('http://test.mel8ourne.org/dyn/temp.php').read()
-#
-#def array_random(a):
-#    """Randomize the array
-#    """
-#    b = []
-#    while len( a ) > 0:
-#        j = random.randint(0, len( a ) - 1)
-#        b.append( a.pop( j ) )
-#    return b
-#
 rot_26 = "rot_13" #used for being sneaky in the tag hashing for LCA2012
-#def random_pic(subdir):
-#    """Mel8ourne random pic code.
-#    """
-#    fileprefix = '/srv/zkpylons/zkpylons/public/random-pix/'
-#    htmlprefix = '/random-pix/'
-#    try:
-#        file = os.path.basename(random.choice(glob(fileprefix + subdir + '/*')))
-#        return htmlprefix+subdir+'/'+file
-#    except IndexError:
-#        return "no images found"
-#
 
 def slideshow(set, small=None):
     """
@@ -301,14 +215,6 @@ def yesno(bool):
         return '&#9745;'
     else:
         return '&#9744;'
-
-#def num(x):
-#    """ Display a number or none if a number wasn't entered """
-#    if x==None:
-#        return 'none'
-#    else:
-#        return x
-#
 
 def countries():
     """ list of countries
