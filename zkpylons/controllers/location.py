00a18a056942fcd16aa6a2cab7b601d1e1d5f333
James Iseppi <james@iseppi.org>
2014-01-01 23:59:36 +1000

    * Add ical feed for each room

--- zkpylons/controllers/location.py
+++ zkpylons/controllers/location.py
@@ -1,4 +1,5 @@
 import logging
+import vobject
 
 from pylons import request, response, session, tmpl_context as c
 from zkpylons.lib.helpers import redirect_to
@@ -41,14 +42,15 @@ class EditLocationSchema(BaseSchema):
 class LocationController(BaseController):
 
     @enforce_ssl(required_all=True)
-    @authorize(h.auth.has_organiser_role)
     def __before__(self, **kwargs):
         c.can_edit = True
 
+    @authorize(h.auth.has_organiser_role)
     @dispatch_on(POST="_new")
     def new(self):
         return render('/location/new.mako')
 
+    @authorize(h.auth.has_organiser_role)
     @validate(schema=NewLocationSchema(), form='new', post_only=True, on_get=True, variable_decode=True)
     def _new(self):
         results = self.form_result['location']
@@ -60,14 +62,17 @@ class LocationController(BaseController):
         h.flash("Location created")
         redirect_to(action='index', id=None)
 
+    @authorize(h.auth.has_organiser_role)
     def view(self, id):
         c.location = Location.find_by_id(id)
         return render('/location/view.mako')
 
+    @authorize(h.auth.has_organiser_role)
     def index(self):
         c.location_collection = Location.find_all()
         return render('/location/list.mako')
 
+    @authorize(h.auth.has_organiser_role)
     @dispatch_on(POST="_edit")
     def edit(self, id):
         c.location = Location.find_by_id(id)
@@ -77,6 +82,7 @@ class LocationController(BaseController):
         form = render('/location/edit.mako')
         return htmlfill.render(form, defaults)
 
+    @authorize(h.auth.has_organiser_role)
     @validate(schema=EditLocationSchema(), form='edit', post_only=True, on_get=True, variable_decode=True)
     def _edit(self, id):
         location = Location.find_by_id(id)
@@ -89,6 +95,7 @@ class LocationController(BaseController):
         h.flash("The Location has been updated successfully.")
         redirect_to(action='index', id=None)
 
+    @authorize(h.auth.has_organiser_role)
     @dispatch_on(POST="_delete")
     def delete(self, id):
         """Delete the location
@@ -100,6 +107,7 @@ class LocationController(BaseController):
         c.location = Location.find_by_id(id)
         return render('/location/confirm_delete.mako')
 
+    @authorize(h.auth.has_organiser_role)
     @validate(schema=None, form='delete', post_only=True, on_get=True, variable_decode=True)
     def _delete(self, id):
         c.location = Location.find_by_id(id)
@@ -108,3 +116,49 @@ class LocationController(BaseController):
 
         h.flash("Location has been deleted.")
         redirect_to('index')
+
+    def ical(self, id):
+        c.schedule_collection = Location.find_by_id(id).schedule
+
+        ical = vobject.iCalendar()
+        for schedule in c.schedule_collection:
+            if not schedule.time_slot.heading:
+                event = ical.add('vevent')
+                event.add('uid').value = str(schedule.id) + '@' + h.lca_info['event_host']
+                # Created
+                event.add('created').value = schedule.creation_timestamp.replace(tzinfo=h.lca_info['time_zone'])
+                # Last Modified
+                event.add('dtstamp').value = schedule.last_modification_timestamp.replace(tzinfo=h.lca_info['time_zone'])
+                event.add('last-modified').value = schedule.last_modification_timestamp.replace(tzinfo=h.lca_info['time_zone'])
+                # Start and End Time
+                event.add('dtstart').value = schedule.time_slot.start_time.replace(tzinfo=h.lca_info['time_zone'])
+                event.add('dtend').value = schedule.time_slot.end_time.replace(tzinfo=h.lca_info['time_zone'])
+                # Title and Author (need to add Author here)
+                event.add('summary').value = schedule.event.computed_title() + '. ' + h.list_to_string(schedule.event.computed_speakers())
+                # Abstract, if we have one
+                event.add('description').value = schedule.event.computed_abstract()
+                # Add a URL
+                if schedule.event.proposal:
+                    event.add('url').value = h.url_for(qualified=True, controller='schedule', action='view_talk', id=schedule.event.proposal.id)
+                elif not (schedule.event.url is None or schedule.event.url == ''):
+                    if schedule.event.url.startswith('https://') or schedule.event.url.startswith('http://'):
+                        event.add('url').value = h.url_for(str(schedule.event.url))
+                    else:
+                        event.add('url').value = h.url_for(str(schedule.event.url), qualified=True)
+
+                concurrent_schedules = schedule.event.schedule_by_time_slot(schedule.time_slot)
+                for concurrent_schedule in concurrent_schedules:
+                    if concurrent_schedule != schedule:
+                        if concurrent_schedule in c.schedule_collection:
+                            c.schedule_collection.remove(concurrent_schedule)
+
+                locations = [concurrent_schedule.location.display_name for concurrent_schedule in concurrent_schedules]
+                event.add('location').value = h.list_to_string(locations)
+
+        response.charset = 'utf8'
+        response.headers['content-type'] = 'text/calendar; charset=utf8'
+        response.headers.add('content-transfer-encoding', 'binary')
+        response.headers.add('Pragma', 'cache')
+        response.headers.add('Cache-Control', 'max-age=3600,public')
+        return ical.serialize()
+
